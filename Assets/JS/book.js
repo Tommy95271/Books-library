9b6c07ad37feba08a3906cc0c6ac26b1043d49fc
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 14:58:42 +0800

    If you want to see text and variable in one line, the better way is to use comma separate them.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -1,28 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const bookList = document.querySelector('#book-list');
-	console.log(`book list node type: ${bookList.nodeType}`);
-	console.log(`book list node name: ${bookList.nodeName}`);
-	console.log(`book list parent element: ${bookList.parentElement}`);
-	console.log(`book list parent node: ${bookList.parentNode}`);
-
-	console.log(`all node children: ${Array.from(bookList.childNodes)}`);
-	console.log(Array.from(bookList.childNodes));
-	Array.from(bookList.childNodes).forEach(function(node) {
-		console.log(node);
-	});
-
-	console.log(`all element children ${Array.from(bookList.children)}`);
-	console.log(Array.from(bookList.children));
-	Array.from(bookList.children).forEach(function(node) {
-		console.log(node);
-	});
-
-	const titles = document.querySelectorAll('.name');
-	console.log(`${titles}`);
-	console.log(titles);
-	console.log(`Book titles ${titles}`);
-	console.log('Bookd titles' + titles);
-	Array.from(titles).forEach(function(title) {
-		console.log(title);
-	});
+	console.log('#book-list next silbing', bookList.nextSibling);
 });
