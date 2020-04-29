714b6aa376e58243c487c8498235a7fd59169960
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:33:21 +0800

    This is a better way to remove element, simpler and easier.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -1,21 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const listItems = document.querySelectorAll('#book-list ul li .delete');
-	console.log(listItems);
-	Array.from(listItems).forEach(function(item) {
-		item.addEventListener('click', (e) => {
+	const list = document.querySelector('#book-list ul');
+	console.log(list);
+
+	//delete books in easier way
+	list.addEventListener('click', (e) => {
+		if (e.target.className === 'delete') {
 			const li = e.target.parentElement;
-			console.log('child element to remove', li);
-			console.log('parent element to remove child from', li.parentElement);
 			li.parentElement.removeChild(li);
-		});
-	});
-
-	//prevent default behavior
-
-	const link = document.querySelector('#page-banner a');
-	console.log(link);
-	link.addEventListener('click', function(e) {
-		e.preventDefault();
-		console.log(link);
+		}
 	});
 });
