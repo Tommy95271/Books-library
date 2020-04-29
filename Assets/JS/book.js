5175c5b0943f87f6c31c50561888b98f122c64bd
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:03:28 +0800

    You can do this, too.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -4,4 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	console.log('#book-list next element silbing', bookList.nextElementSibling);
 	console.log('#book-list previous silbing', bookList.previousSibling);
 	console.log('#book-list previous element silbing', bookList.previousElementSibling);
+
+	bookList.previousElementSibling.querySelector('p').innerHTML += "<h1>It's a inserted h1</h1>";
 });
