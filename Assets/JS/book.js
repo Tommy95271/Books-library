9545026ff2f269ddfd6fa5fe18400588fbdf3778
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:00:10 +0800

    As before, bookList.nextSibling will return Enter, so you need to use bookList.nextElementSibling to get element.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const bookList = document.querySelector('#book-list');
 	console.log('#book-list next silbing', bookList.nextSibling);
+	console.log('#book-list next element silbing', bookList.nextElementSibling);
 });
