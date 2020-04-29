119481111fed402a547c480464f421533832485b
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:01:44 +0800

    If nextSibling exists, so does previousSibling.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -2,4 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	const bookList = document.querySelector('#book-list');
 	console.log('#book-list next silbing', bookList.nextSibling);
 	console.log('#book-list next element silbing', bookList.nextElementSibling);
+	console.log('#book-list previous silbing', bookList.previousSibling);
+	console.log('#book-list previous element silbing', bookList.previousElementSibling);
 });
