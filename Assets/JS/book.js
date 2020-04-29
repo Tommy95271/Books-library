ffc2a29dae5a136ae2b36a643f8bdee9e3bfcb75
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 11:52:21 +0800

    Grab element by querySelector and querySelectorAll are Node List.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -2,4 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	var bookList = document.getElementById('book-list');
 	var titles = document.getElementsByClassName('name');
 	console.log(bookList, titles);
+	var titles2 = document.querySelectorAll('#book-list li .name');
+	console.log(titles2);
 });
