63fbfe7fcbfc5ed729582a68b175fa8bbe1da186
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:39:23 +0800

    you can get it after reading the comments.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	const list = document.querySelector('#book-list ul');
 	console.log(list);
 
-	//delete books in easier way
+	// delete books in easier way
 	list.addEventListener('click', (e) => {
+		// if the target (span) you click has class 'delete'
 		if (e.target.className === 'delete') {
+			// get the span's parent which is li
 			const li = e.target.parentElement;
+			// get li's parent which is ul and delete the whole li
 			li.parentElement.removeChild(li);
 		}
 	});
