3c3bffe4b36fba6b4a2e7d2de8920200de0b69e6
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-04-29 15:21:45 +0800

    A way to remove li when you click delete button.

--- Assets/JS/book.js
+++ Assets/JS/book.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const bookList = document.querySelector('#book-list');
-	console.log('#book-list next silbing', bookList.nextSibling);
-	console.log('#book-list next element silbing', bookList.nextElementSibling);
-	console.log('#book-list previous silbing', bookList.previousSibling);
-	console.log('#book-list previous element silbing', bookList.previousElementSibling);
-
-	bookList.previousElementSibling.querySelector('p').innerHTML += "<h1>It's a inserted h1</h1>";
+	const listItems = document.querySelectorAll('#book-list ul li .delete');
+	console.log(listItems);
+	Array.from(listItems).forEach(function(item) {
+		item.addEventListener('click', (e) => {
+			const li = e.target.parentElement;
+			console.log('child element to remove', li);
+			console.log('parent element to remove child from', li.parentElement);
+			li.parentElement.removeChild(li);
+		});
+	});
 });
